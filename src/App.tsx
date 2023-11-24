c6fd8aac4d5bcd89ff45599e1ca5501831e4edf4
yena <jntantmsemt@naver.com>
2023-11-25 00:22:20 +0900

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+function App() {
+  return (
+    <div>
+      <p>로고</p>
+      <div>
+        <div>portfolio</div>
+        <div>about me</div>
+        <div>skills</div>
+        <div>contact</div>
+      </div>
+      <div>login</div>
+    </div>
+  );
+}
+
+export default App;
